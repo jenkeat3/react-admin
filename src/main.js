9e637d86f9842ce09126b17975cc2cc52b478299
jk <initkeat@gmail.com>
2015-12-13 23:37:40 +0800

    setup env and main page

---
+++ src/main.js
@@ -0,0 +1,8 @@
+$ = jQuery = require('jquery');
+var React = require('react');
+var Home = require('./components/homepage');
+var About = require('./components/aboutpage');
+
+
+
+React.render(<Home />, document.getElementById('app'));
